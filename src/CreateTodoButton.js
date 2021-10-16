7c21b34c8b9982306111a7052726e8f90d46844b
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-16 17:45:59 -0500

    Agregando eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./CreateTodoButton.css";
 
-function CreateTodoButton(){
+function CreateTodoButton(props){
+    const onClickButton = (msg) => {
+        alert(msg);
+    }
     return(
         <button 
         className="CreateTodoButton"
-        
+        onClick={() => onClickButton('Aqui deberia aparecer el modal')} //tenemos que envolver una funcion lo que esperamos que se ejecute cuando ocurra el evento
         >
             +
         </button>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,25 @@ import React from "react";
 import  "./TodoItem.css";
 
 function TodoItem (props){
+    const onComplete = () => {
+        alert('Ya completaste el TODOs ' + props.text);
+    };
+    const onDelete = () => {
+        alert('borraste el TODOs ' + props.text);
+    };
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check-active'}`}>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check-active'}`}
+            onClick={onComplete}
+            >
                 ᄼ
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p-completed'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">
+            <span className="Icon Icon-delete"
+            onClick = {onDelete}
+            >
                 X
             </span>
         </li>
