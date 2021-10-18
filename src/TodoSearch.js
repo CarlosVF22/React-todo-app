5738cbd70fcfa5b4bf76767963d920751414e945
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-18 11:36:30 -0500

    Agregando estado al componente TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,16 +3,31 @@ import './TodoSearch.css';
 
 
 function TodoSearch (){
+
+    //agregando estado a nuestro componente
+    //React.useState nos devuelve un array de 2 posiciones [state, setState()]
+    //cuando llamemos a la funcuon setState re Renderizamos nuestro componente con el nuevo estado
+
+    //con setSearchValue actualizamos el valor del estado
+    const [searchValue,setSearchValue] = React.useState(''); //creamos el estado y enviamos el estado inicial que guardamos en la
+    //variable searchValue
+
+    //cada vez que el usuario realice un cambio llamamos a la funcion setSearchValue
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
-    return (
+    return [
         <input 
         className="TodoSearch" 
         placeholder="Cebolla"
+        //conectando el compoente con el valor del estado
+        value={searchValue}
+        //al realizar un cambio ejecutar la funcion onSearchValueChange
         onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export {TodoSearch};
