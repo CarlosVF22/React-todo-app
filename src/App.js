db3fe2a1dd5e8992b2fe3ceeb296da91d1ce595c
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-18 20:47:34 -0500

    Contando TODOs

--- src/App.js
+++ src/App.js
@@ -9,22 +9,45 @@ import { TodoItem } from "./TodoItem";
 
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   {text:'cortar cebollas', completed:false },
   {text:'Tomar el curso de react', completed:true },
-  {text:'llorar con la llorona', completed:false },
-
-  
+  {text:'llorar con la llorona', completed:true },
 ];
 
 function App() { //los componentes comienzan con MAYUSCULA
+
+    //agregando estado a nuestro componente
+    //React.useState nos devuelve un array de 2 posiciones [state, setState()]
+
+    //agregando state a los TODOs
+    const [todos,setTodos] = React.useState(defaultTodos);
+
+    //cuando llamemos a la funcion setState re Renderizamos nuestro componente con el nuevo estado
+    //con setSearchValue actualizamos el valor del estado
+    const [searchValue,setSearchValue] = React.useState(''); //creamos el estado y enviamos el estado inicial que guardamos en la
+    //variable searchValue
+
+    //contando TODOs totales y TODOs completed
+    //utilizamos ! para verificar que la variable es falsa y doble !! es para verificar si es verdadera
+    //cuales todos tienen la propiedad completed como true
+    const completedTodos = todos.filter(todo => !!todo.completed).length;
+    const totalTodos = todos.length;
+
   return (
     // React.Fragment es una etiqueta invisible que contiene nuestros componentes
     //solo se puede enviar una etiqueta por componente
     <React.Fragment> 
-      <TodoCounter />
+      <TodoCounter 
+        total = {totalTodos}
+        completed = {completedTodos}
+      />
 
-      <TodoSearch />
+      <TodoSearch 
+        //agregamos las props que seran enviadas al componente TodoSearch
+        searchValue ={searchValue}
+        setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
         {todos.map(todo =>(
@@ -42,3 +65,5 @@ function App() { //los componentes comienzan con MAYUSCULA
 }
 
 export default App;
+//filtrar la cantidad de TODOs dependiendo en searchValue la cantidad de TODO que aparecen 
+//palabras semejantes
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,22 +2,16 @@ import React from "react";
 import './TodoSearch.css';
 
 
-function TodoSearch (){
-
-    //agregando estado a nuestro componente
-    //React.useState nos devuelve un array de 2 posiciones [state, setState()]
-    //cuando llamemos a la funcuon setState re Renderizamos nuestro componente con el nuevo estado
-
-    //con setSearchValue actualizamos el valor del estado
-    const [searchValue,setSearchValue] = React.useState(''); //creamos el estado y enviamos el estado inicial que guardamos en la
-    //variable searchValue
+//Actualizar estado por medio de props
+//el componente que llame al componente TodoSEarch debe enviar 2 props ({searchValue,setSearchValue})
+function TodoSearch ({searchValue,setSearchValue}){
 
     //cada vez que el usuario realice un cambio llamamos a la funcion setSearchValue
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
     }
-    return [
+    return (
         <input 
         className="TodoSearch" 
         placeholder="Cebolla"
@@ -25,9 +19,8 @@ function TodoSearch (){
         value={searchValue}
         //al realizar un cambio ejecutar la funcion onSearchValueChange
         onChange={onSearchValueChange}
-        />,
-        <p>{searchValue}</p>
-    ];
+        />
+    );
 }
 
 export {TodoSearch};
