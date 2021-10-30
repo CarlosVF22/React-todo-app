8be27612d65a121746d7998a7eb0dc67f5da3bb7
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-30 10:01:48 -0500

    Creando el primer High Order Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/index.js';
+// import App from './App/index.js';
+
+
+function App(props){
+    return(
+        <h1>¡{props.saludo}, {props.nombre}!</h1>
+    );
+}
+
+// siempre el retorno de la ultima funcion dentro del HOC tiene que ser un componente de React
+// Utilizando high Order Function (HOC)
+// Se utiliza with por convencion
+function withSaludo (WrappedComponent){
+    return function WrappedComponentWithSaludo(saludo) {
+        return function ComponenteDeVerdad(props) {
+            return (
+                <React.Fragment>
+                    <WrappedComponent {...props} saludo={saludo}/>
+                    <p>Estamos acompañando al Al WrappedComponent</p>
+                </React.Fragment>
+            );
+        }
+    }
+}
+
+const AppWithSaludo = withSaludo(App)('Hola');
 
 
 ReactDOM.render( //renderizando el componente app
-    <App />,
+    <AppWithSaludo nombre="Juanita"/>,
+    // <App saludo="Buenas" nombre="Nath" />,
     document.getElementById("root")
 );
 
